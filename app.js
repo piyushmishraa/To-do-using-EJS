286611a572853493e8446c1562a30f2d7d5d96ae
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-04-25 18:37:33 +0530

    installed ejs

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ const bodyParser = require("body-parser");
 const express=require("express");
 
 const app=express();
+app.set('view engine', 'ejs');
 
-app.listen(3000,function(){
+
+app.get("/",function(){
+var today = new Date();
+ var currentDay=today.getDay();
+ var day="";
+
+if(currentDay===0||currentDay===6){
+  day="weekend";
+}
+else{
+    day="weekday";
+};
+
+
+app.get('/', (req, res) => {
+    res.render('list', {kindofday: day});
+  });
+
+
+})
+
+app.listen(4000,function(){
     console.log("server is up and running");
 });
