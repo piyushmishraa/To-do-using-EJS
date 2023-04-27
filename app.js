98a4ab95cf202a3cba687064a15c5ea0fbf42eb2
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-04-27 17:36:51 +0530

    added a bit of css

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 var items=["eat","sleep","code","repeat"];
+app.use(express.static("public"))
 app.get("/", function (req, res) {
   var today = new Date();
   
