0d9560d52a31889f58815b44b66d48f26e96100d
Piyush Mishra <fanboiofharrystyles@gmail.com>
2023-05-25 22:52:49 +0530

    step 1 of adding a  database done.

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const date=require(__dirname + "/date.js");
 
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model('Item', itemsSchema);
+
+const item1 = new Item({
+   name: 'Welcome to-do list'
+   });
+
+const item2 =new Item({
+  name: "Hit + to add a nwe item"
+}) ;   
+
+const item3=new Item({
+  name:"press <--- to delete an item"
+});
+
+const defaultItems=[item1,item2,item3];
+
+Item.insertMany(defaultItems)
+      .then(function () {
+        console.log("Successfully saved defult items to DB");
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
-var items=[];
-var workitems=[];
+
 app.use(express.static("public"))
 app.get("/", function (req, res) {
 
-  let  day = date.getDate();
 
   res.render("list", { listTitle: day,newitems:items });
 });
